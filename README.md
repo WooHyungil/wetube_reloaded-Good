45a64f7c57a821657b381e6fbbd08deb540070ad
WooHyungil <github-email>
2023-02-16 07:09:46 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Wetube Reloaded
+
+/ -> Home
+
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/:id -> See user
+/users/logout -> Log Out
+/users/edit -> Edit MY Profile
+/users/delete -> Delete MY Profile
+
+/videos/:id -> See Video
+/videos/:id/edit -> Edit Video
+/videos/:id/delete -> Delet Video
+/videos/upload -> Upload Video
+
+//라우터는 /users를 만들고 URL을 추가해 나가도록 해준다
---
+++ src/server.js
@@ -0,0 +1,24 @@
+import "./db";
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
+const PORT = 6002;
+
+const app = express();
+const logger = morgan("dev"); // 다른 옵션도 있다. 서버의 요청시간, 속도 등등....
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use(express.urlencoded({ extended: true })); //<-- from의 value들을 이해할 수 있도록 하고,자바스크립트 형식으로 변형시켜준다
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/videos", videoRouter);
+
+const handleListening = () =>
+  console.log(`✅ Server listention on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
