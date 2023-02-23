8a1c9e21ab0a03f033a31f475d8be47aae8c287a
ellie-shim <hiss0723@naver.com>
2023-02-24 05:04:17 +0900

    update

--- src/init.js
+++ src/init.js
@@ -1,8 +1,9 @@
 import "./db";
 import "./models/Video";
+import "./models/User";
 import app from "./server";
 
-const PORT = 6000;
+const PORT = 8843;
 
 const handleListening = () =>
   console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import "./models/Video";
 
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
@@ -14,7 +14,7 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true })); //<-- from의 value들을 이해할 수 있도록 하고,자바스크립트 형식으로 변형시켜준다
-app.use("/", globalRouter);
+app.use("/", rootRouter);
 app.use("/user", userRouter);
 app.use("/videos", videoRouter);
 
